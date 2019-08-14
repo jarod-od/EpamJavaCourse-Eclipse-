ce864a1551a537295de03701a5dafa97829b8ece
Jarod <konstantin.zalevsky@gmail.com>
2019-08-14 23:27:23 +0300

    test

--- epam-training-java-fundamentals/src/com/jarod/epam/lessons/LetStart.java
+++ epam-training-java-fundamentals/src/com/jarod/epam/lessons/LetStart.java
@@ -1,21 +1,14 @@
 package com.jarod.epam.lessons;
 
 public class LetStart {
-	static final int DOG = 2;
-	
-	public static void main (String[] args) {
-		System.out.println("Hello World!!!");
-		LetStart boom = new LetStart();
-		int speed = 13;
-		setSpeed();
-		
-		
-		
-//		test commit2
-	}
 
-	private static void setSpeed() {
-		// TODO Auto-generated method stub ;[
+	public static void main (String[] args) {
 		
+	byte a = 111;
+	char ch = 100;
+	
+	System.out.println(ch);
+
 	}
 }
+	
