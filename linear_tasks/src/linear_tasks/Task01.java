600d559fbf5beb05df0792df46bc20ffeebb71e5
Jarod <konstantin.zalevsky@gmail.com>
2019-08-17 23:09:26 +0300

    старт

---
+++ linear_tasks/src/linear_tasks/Task01.java
@@ -0,0 +1,42 @@
+package linear_tasks;
+
+import java.util.Scanner;
+
+// Даны два действительных числа х и у.
+// Вычислить их сумму, разность, произведение и частное.
+
+public class Task01 {
+	/*
+	 * private int x; private int y;
+	 * 
+	 * public int getX() { return x; }
+	 * 
+	 * public void setX(int x) { this.x = x; }
+	 * 
+	 * public int getY() { return y; }
+	 * 
+	 * public void setY(int y) { this.y = y; }
+	 */
+
+	public static void main(String[] args) {
+		/*
+		 * Task01 exmpl = new Task01(); 
+		 * exmpl.setX(100); 
+		 * exmpl.setY(200);
+		 * System.out.println("Sum = " + (exmpl.getX() + exmpl.getY()) );
+		 */
+		int x;
+		int y;
+		
+	System.out.println("Input x, y: ");
+	
+	Scanner s = new Scanner(System.in);
+	x = s.nextInt();
+	y = s.nextInt();
+	System.out.println("Sum of x + y = " + (x + y) );
+	System.out.println("Difference of x + y = " + (x - y));
+	System.out.println("Multi of x + y = " + (x * y));
+	System.out.println("Divide of x + y = " + (x / y));
+	}
+
+}
---
+++ linear_tasks/src/linear_tasks/Task03.java
@@ -0,0 +1,18 @@
+package linear_tasks;
+
+// Найдите значение функции:
+// z = 2 * x + (y - 2) * 5
+
+public class Task03 {
+
+	public static void main(String[] args) {
+		int x = 5;
+		int y = 7;
+
+		int z;
+
+		z = 2 * x + (y - 2) * 5;
+		System.out.println("z = " + z);
+
+	}
+}
