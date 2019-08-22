9651522380d80a0cb5be142df07ff2d129dc21ab
Jarod <konstantin.zalevsky@gmail.com>
2019-08-22 23:25:19 +0300

    линеечки пилим

---
+++ linear_tasks/src/linear_tasks/Task13.java
@@ -0,0 +1,15 @@
+package linear_tasks;
+
+// Заданы координаты трех вершин треугольника(x1 y1),(x2 y2),(x3 y3). Найти его периметр и площадь
+
+import java.util.Scanner;
+
+public class Task13 {
+	public static void main(String[] args) {
+		
+		
+		
+		
+		
+	}
+}
