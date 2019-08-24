7834fd8bae6be69e7821878c8d74e91a0934dad8
Jarod <konstantin.zalevsky@gmail.com>
2019-08-24 23:44:37 +0300

    Запилили биг тасочку на координаты
    Пилим дальше

---
+++ linear_tasks/src/linear_tasks/Task15.java
@@ -0,0 +1,12 @@
+package linear_tasks;
+
+// Написать программу, которая выводит на экран первые четыре степени числа Пи
+
+public class Task15 {
+	public static void main(String[] args) {
+		
+		System.out.println("Pi^0,1,2,3");
+		
+		System.out.println("Power of Pi is: "); //Math.pow(Math.PI, 0, 1, 2, 3));
+	}
+}
