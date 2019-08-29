8f0e27e816e3e0e7ff722aedb86c4a0f2035027d
Jarod <konstantin.zalevsky@gmail.com>
2019-08-29 22:50:37 +0300

    Пошли ветвления

---
+++ linear_tasks/src/od/jarod/epam/condition_tasks/Task02.java
@@ -0,0 +1,17 @@
+package od.jarod.epam.condition_tasks;
+
+/*
+ * Составить программу сравнения двух чисел 1 и 2. 
+ * Если 1 меньше 2 - вывести на экран слово "yes", в противном случае слово "no"
+ */
+
+public class Task02 {
+	
+	public static void main (String[] args) {
+		
+		
+		
+		
+		
+	}
+}
