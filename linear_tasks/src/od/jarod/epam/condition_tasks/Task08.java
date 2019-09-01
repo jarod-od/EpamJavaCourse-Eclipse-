9f4fd49258810f62642348b4a131905215b760ea
Jarod <konstantin.zalevsky@gmail.com>
2019-09-01 21:46:34 +0300

    Ветвления

---
+++ linear_tasks/src/od/jarod/epam/condition_tasks/Task08.java
@@ -0,0 +1,19 @@
+package od.jarod.epam.condition_tasks;
+
+//Составить программу нахождения наименьшего  из квадратов двух чисел a и b
+
+import java.util.Scanner;
+
+public class Task08 {
+
+	public static void main (String[] args) {
+		
+		int a;
+		int b;
+		
+		
+		
+		
+		
+	}
+}
