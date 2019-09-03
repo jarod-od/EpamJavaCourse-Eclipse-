e9fd731648c85fde7b22f42c99c7cd47f0575a2a
Jarod <konstantin.zalevsky@gmail.com>
2019-09-03 22:58:58 +0300

    ифы изи

--- linear_tasks/src/od/jarod/epam/condition_tasks/Task08.java
+++ linear_tasks/src/od/jarod/epam/condition_tasks/Task08.java
@@ -8,12 +8,29 @@ public class Task08 {
 
 	public static void main (String[] args) {
 		
-		int a;
-		int b;
+		double a;
+		double b;
+		
+		while (true) {
+			try {
+				System.out.println("Pls input a & b: ");
+				
+				Scanner sc = new Scanner(System.in);
+				a = sc.nextDouble();
+				b = sc.nextDouble();
+				
+				System.out.println("a is: " + a + " and b is: " + b);
+				
+				break;
+				
+			} catch (Exception e) {
+				System.out.println("Invalid input, pls try again");
+			}
+		}
 		
 		
-		
-		
-		
+		String s;
+		s = (Math.sqrt(a) < Math.sqrt(b)) ? "a" : "b";
+		System.out.println("Smaller square number is: " + s);
 	}
 }
---
+++ linear_tasks/src/od/jarod/epam/condition_tasks/Task10.java
@@ -0,0 +1,20 @@
+package od.jarod.epam.condition_tasks;
+
+// Составить программу, которая определит площадь какого круга меньше.
+
+import java.util.Scanner;
+
+public class Task10 {
+
+	public static void main (String[] args) {
+		
+		double radius1;
+		double radius2;
+		
+		
+		
+		
+		
+		
+	}
+}
