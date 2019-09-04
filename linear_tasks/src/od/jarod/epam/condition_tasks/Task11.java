6b91a32eade6d97141b904523332870ed9abaf21
Jarod <konstantin.zalevsky@gmail.com>
2019-09-04 23:15:32 +0300

    Слушаю лекцию и пишу ифы

---
+++ linear_tasks/src/od/jarod/epam/condition_tasks/Task11.java
@@ -0,0 +1,20 @@
+package od.jarod.epam.condition_tasks;
+
+/*
+ *  Даны три действительных числа. Возвести  в квадрат те из них, значения которых неотрицательны, 
+ *  и в четвертую степень - отрицательные
+*/
+
+import java.util.Scanner;
+
+public class Task11 {
+
+	public static void main(String[] args) {
+		
+		
+		
+		
+		
+		
+	}
+}
