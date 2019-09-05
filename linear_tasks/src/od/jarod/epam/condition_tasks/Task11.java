db8b23863a5170212f575dbb96eb6039fe4e4e9d
Jarod <konstantin.zalevsky@gmail.com>
2019-09-05 22:25:45 +0300

    Повозился с ифами, думал общие сделать, не вышло.

--- linear_tasks/src/od/jarod/epam/condition_tasks/Task11.java
+++ linear_tasks/src/od/jarod/epam/condition_tasks/Task11.java
@@ -10,11 +10,51 @@ import java.util.Scanner;
 public class Task11 {
 
 	public static void main(String[] args) {
-		
-		
-		
-		
-		
-		
+
+		double a;
+		double b;
+		double c;
+
+		while (true) {
+			try {
+
+				System.out.println("Pls input a, b, c: ");
+
+				Scanner s = new Scanner(System.in);
+				a = s.nextDouble();
+				b = s.nextDouble();
+				c = s.nextDouble();
+
+				System.out.println("a is: " + a + ", b is: " + b + ", c is: " + c);
+
+				break;
+
+			} catch (Exception e) {
+				System.out.println("Invalid input, pls try again");
+			}
+		}
+
+		if (a >= 0) {
+			System.out.println("a is: " + Math.pow(a, 2));
+			
+		} else if (a < 0) {
+			System.out.println("a is: " + Math.pow(a, 4));
+		}
+
+		if (b >= 0) {
+			System.out.println("b is: " + Math.pow(b, 2));
+			
+		} else if (b < 0) {
+			System.out.println("b is: " + Math.pow(b, 4));
+		}
+
+		if (c >= 0) {
+			System.out.println("c is: " + Math.pow(c, 2));
+			
+		} else if (c < 0) {
+			System.out.println("c is: " + Math.pow(c, 4));
+
+		}
+
 	}
 }
