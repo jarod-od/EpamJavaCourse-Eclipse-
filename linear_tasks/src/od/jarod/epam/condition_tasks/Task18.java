fb6fd2b502a0f4e7bdabd493712a138bdfdb0d13
Jarod <konstantin.zalevsky@gmail.com>
2019-09-14 21:38:50 +0300

    Лекция по ветвлениям. Switch

---
+++ linear_tasks/src/od/jarod/epam/condition_tasks/Task18.java
@@ -0,0 +1,20 @@
+package od.jarod.epam.condition_tasks;
+
+// Подсчитать количество отрицательных чисел среди a, b, c.
+
+import java.util.Scanner;
+
+public class Task18 {
+	
+	public static void main(String[] args) {
+		
+//		TODO 
+		
+		
+		
+		
+		
+		
+		
+	}
+}
