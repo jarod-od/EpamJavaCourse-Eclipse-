02e1356416c5abdb4af090190aec79a42c540718
Jarod <konstantin.zalevsky@gmail.com>
2019-09-18 23:36:19 +0300

    циклы. Не закомиталось.

--- linear_tasks/src/od/jarod/epam/loop_tasks/Task03.java
+++ linear_tasks/src/od/jarod/epam/loop_tasks/Task03.java
@@ -3,7 +3,6 @@ package od.jarod.epam.loop_tasks;
 // Необходимо вывести на экран таблицу умножения на 3.
 
 public class Task03 {
-	
 
 	public static void main(String[] args) {
 		
@@ -11,7 +10,7 @@ public class Task03 {
 		
 		for (int i = 1; i <= 10; i++) {
 			
-			System.out.println("3 * " + i + " = " + 3*i);
+			System.out.println("3 * " + i + " = " + 3 * i);
 
 		}
 	}
