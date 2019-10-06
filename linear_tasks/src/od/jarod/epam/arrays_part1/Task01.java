23541fe5047e1f4781ed6ef88042e843509f37be
Jarod <konstantin.zalevsky@gmail.com>
2019-10-06 22:20:05 +0300

    Лекция, повтор основ

---
+++ linear_tasks/src/od/jarod/epam/arrays_part1/Task01.java
@@ -0,0 +1,14 @@
+package od.jarod.epam.arrays_part1;
+
+//В массив А[N] занесены натуральные числа. Найти сумму тех элементов, которые кратны данному К.
+
+public class Task01 {
+
+	public static void main(String[] args) {
+		
+		
+		
+		
+		
+	}
+}
