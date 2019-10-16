fc43db8d6dc7b7a1a6ad097637fd582d0862f414
Jarod <konstantin.zalevsky@gmail.com>
2019-10-16 21:43:54 +0300

    чудеса гита

--- practice/src/lecture_code/Arrays.java
+++ practice/src/lecture_code/Arrays.java
@@ -1,5 +1,35 @@
 package lecture_code;
 
 public class Arrays {
-//TODO
+
+		public static void main(String[] args) {
+
+			int[] arr = new int[4];
+			arr[0] = 2;
+			arr[2] = 99;
+			
+			for(int i = 0; i < arr.length; i++) {
+				arr[i] = i+1;
+				
+				System.out.println(i);
+			}
+			
+			System.out.println(arr[2]);
+			System.out.println(arr[0]);
+			
+			
+			
+			
+			
+			
+			boolean[] bool = new boolean[2];
+			System.out.println(bool[1]);
+
+			
+			int[] arr2 = {1, 4, -3, 0};
+
+			
+		}
+	}
+
 }
